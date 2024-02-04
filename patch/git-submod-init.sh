90c370230bc30fe79d4c8665db3844c2dbe2b0b5
Rene Rivera <grafikrobot@gmail.com>
2024-02-03 22:04:21 -0600

    Tweak util scripts after testing.

--- patch/git-submod-init.sh
+++ patch/git-submod-init.sh
@@ -9,10 +9,15 @@ rootdir=`pwd`
 echo "*****" `pwd`
 subdir=${1}
 submod=`basename ${subdir}`
-git submodule init "${subdir}"
+echo git submodule update --init --force "${subdir}"
+git submodule update --init --force "${subdir}"
 cd "${subdir}"
+echo "=====" `pwd`
+echo git remote add grafikrobot.boostorg.${submod} https://github.com/grafikrobot/boostorg.${submod}.git
 git remote add grafikrobot.boostorg.${submod} https://github.com/grafikrobot/boostorg.${submod}.git
-git fetch
+echo git fetch -p --all
+git fetch -p --all
+echo git switch -c modular grafikrobot.boostorg.${submod}/modular
 git switch -c modular grafikrobot.boostorg.${submod}/modular
 cd "${rootdir}"
 echo "*****" `pwd`
--- patch/patch-apply.sh
+++ patch/patch-apply.sh
@@ -13,11 +13,14 @@ submod=`basename ${subdir}`
 cd "${subdir}"
 git reset --hard
 git pull -r
-if test -s "${patchdir}/${subdir}/patch.diff" ; then
-	patch -uN -p1 "--input=${patchdir}/${subdir}/patch.diff"
+if test "${2}" == "--diff" ; then
+	if test -s "${patchdir}/${subdir}/patch.diff" ; then
+		patch -uN -p1 "--input=${patchdir}/${subdir}/patch.diff"
+	fi
 fi
 cd "${patchdir}/${subdir}"
-find . -name build.jam -exec cp "{}" "${rootdir}/${subdir}/{}" ";"
+find . -name build.jam -exec bash -c "cp {} ${rootdir}/${subdir}/{}" ";"
+find . -name build.jam -exec bash -c "cd ${rootdir}/${subdir} && cd \`dirname {}\` && rm -f *amfile*" ";"
 
 cd "${rootdir}"
 echo "*****" `pwd`
