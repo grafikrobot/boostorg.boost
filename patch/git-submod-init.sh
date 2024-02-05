4299b809fdd80f87da9fe7f01309767ca15b4075
Rene Rivera <grafikrobot@gmail.com>
2024-02-05 07:09:00 -0600

    Craete modular branch as part of submod init.

--- patch/git-submod-init.sh
+++ patch/git-submod-init.sh
@@ -9,15 +9,24 @@ rootdir=`pwd`
 echo "*****" `pwd`
 subdir=${1}
 submod=`basename ${subdir}`
-echo git submodule update --init --force "${subdir}"
-git submodule update --init --force "${subdir}"
+echo git submodule update --init "${subdir}"
+git submodule update --init "${subdir}"
 cd "${subdir}"
 echo "=====" `pwd`
-echo git remote add grafikrobot.boostorg.${submod} https://github.com/grafikrobot/boostorg.${submod}.git
-git remote add grafikrobot.boostorg.${submod} https://github.com/grafikrobot/boostorg.${submod}.git
+if test `git remote show` == "origin" ; then
+	echo git remote add grafikrobot.boostorg.${submod} https://github.com/grafikrobot/boostorg.${submod}.git
+	git remote add grafikrobot.boostorg.${submod} https://github.com/grafikrobot/boostorg.${submod}.git
+fi
 echo git fetch -p --all
 git fetch -p --all
-echo git switch -c modular grafikrobot.boostorg.${submod}/modular
-git switch -c modular grafikrobot.boostorg.${submod}/modular
+# Create modular branch from develop.
+if ! git ls-remote --exit-code --heads grafikrobot.boostorg.${submod} refs/heads/modular ; then
+	echo git switch --no-guess --no-track -c modular grafikrobot.boostorg.${submod}/develop
+	git switch --no-guess --no-track -c modular grafikrobot.boostorg.${submod}/develop
+	echo git push --set-upstream grafikrobot.boostorg.${submod} modular:refs/heads/modular
+	git push --set-upstream grafikrobot.boostorg.${submod} modular:refs/heads/modular
+fi
+echo git fetch -p --all
+git fetch -p --all
 cd "${rootdir}"
 echo "*****" `pwd`
