7ea5378834453af9c4fd890210f5a6ebc7c8eb60
Rene Rivera <grafikrobot@gmail.com>
2024-02-25 14:27:30 -0600

    Use bare git for syncs as gh cli is incapable. And add sync of root.

--- patch/git-submod-sync-all.sh
+++ patch/git-submod-sync-all.sh
@@ -20,14 +20,32 @@ while true ; do
 		[[ $l_url =~ url...(.*) ]]
 		l_url=${BASH_REMATCH[1]}
 		echo "===== ${l_name} -- ${l_url} -- ${l_path}"
-		echo gh repo sync grafikrobot/boostorg.${l_name} --branch develop
-		gh repo sync grafikrobot/boostorg.${l_name} --branch develop
-		echo gh repo sync grafikrobot/boostorg.${l_name} --branch modular
-		gh repo sync grafikrobot/boostorg.${l_name} --branch modular
+		echo gh repo sync grafikrobot/boostorg.${l_name} --branch develop --source boostorg/${l_name}
+		gh repo sync grafikrobot/boostorg.${l_name} --branch develop --source boostorg/${l_name}
+		cd "${rootdir}/${l_path}"
+		echo git status -s -b
+		git status -s -b
+		echo git remote add -t develop upstream https://github.com/boostorg/${l_name}.git
+		git remote add -t develop upstream https://github.com/boostorg/${l_name}.git
+		echo git fetch -p upstream
+		git fetch -p upstream
+		echo git checkout modular
+		git checkout modular
+		echo git merge upstream/develop -m "Sync from upstream."
+		git merge upstream/develop -m "Sync from upstream."
+		echo git status -s -b
+		git status -s -b
 	else
 		break
 	fi
 done < "${rootdir}/.gitmodules"
+
+echo "===== boost -- upstream"
+cd "${rootdir}"
+echo git fetch -p upstream
+git fetch -p upstream
+echo git merge upstream/develop -m "Sync from upstream."
+git merge upstream/develop -m "Sync from upstream."
 
 cd "${rootdir}"
 echo "*****" `pwd`
