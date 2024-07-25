0e85680c1fd7410ffadc265368a4b53960ae0e86
Rene Rivera <grafikrobot@gmail.com>
2024-07-25 17:16:33 -0500

    Utility to git push subset of subrepos.

---
+++ patch/git-submod-push-partial.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+cd `dirname ${0}`
+patchdir=`pwd`
+cd ..
+rootdir=`pwd`
+cd "${patchdir}"
+echo "*****" `pwd`
+
+while true ; do
+    if read -r l_name ; then
+        read -r l_path
+        read -r l_url
+        read -r l_frs
+        read -r l_branch
+        [[ $l_name =~ submodule..([a-z_0-9]+) ]]
+        l_name=${BASH_REMATCH[1]}
+        [[ $l_path =~ path...(.*) ]]
+        l_path=${BASH_REMATCH[1]}
+        [[ $l_url =~ url...(.*) ]]
+        l_url=${BASH_REMATCH[1]}
+        case ${l_name} in
+            core | geometry | locale | log | mysql | variant2 | winapi )
+            echo "===== ${l_name} -- ${l_url} -- ${l_path}"
+            cd "${rootdir}/${l_path}"
+            echo "git push"
+            # git push
+            ;;
+        esac
+    else
+        break
+    fi
+done < "${rootdir}/.gitmodules"
+
+echo "===== status"
+cd "${rootdir}"
+git submodule foreach git status -sb
+git status -sb
+
+cd "${rootdir}"
+echo "*****" `pwd`
